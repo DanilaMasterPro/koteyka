bae727b4a5f62a90e80d5896b1dded32e0b7b493
DanilaMasterPro <daniilbortvin@gmail.com>
2020-08-21 16:32:52 +0300

    Закончил со сриптом фильтра

--- app/js/app.js
+++ app/js/app.js
@@ -34,91 +34,226 @@ document.addEventListener("DOMContentLoaded", function() {
 		d.querySelector('.filter').classList.remove('active');
 	})
 
+
+
+
+
+
+	// Товары
+
 	const productData = [
 	  {
 	  	photo: "images/dest/catalog-img-1.jpg",
 	    name: "Эконом",
-	    area: 0,63,
-	    equipment: [none],
+	    area: 0.63,
 	    price: 100
 	  },
 	  {
 	  	photo: "images/dest/catalog-img-2.jpg",
 	    name: "Эконом плюс",
 	    size: '90x100x180',
-	    area: 0,90,
-	    equipment: [lejac, game],
+	    area: 0.90,
+	    equipment: ["Лежак", "Игровой комплекс"],
 	    price: 200
 	  },
 	  {
 	  	photo: "images/dest/catalog-img-3.jpg",
 	    name: "Комфорт",
 	    size: '100x125x180',
-	    area: 1,13,
-	    equipment: [lejac, game, сlaws],
+	    area: 1.13,
+	    equipment: ["Лежак", "Игровой комплекс", "Когтеточка"],
 	    price: 250
 	  },
 	  {
 	  	photo: "images/dest/catalog-img-4.jpg",
 	    name: "Сьют",
 	    size: '125x125x180',
-	    area: 1,56,
-	    equipment: [lejac, game, сlaws],
+	    area: 1.56,
+	    equipment: ["Лежак", "Игровой комплекс", "Когтеточка"],
 	    price: 350
 	  },
 	  {
 	  	photo: "images/dest/catalog-img-5.jpg",
 	    name: "Люкс",
 	    size: '160x160x180',
-	    area: 2,56,
-	    equipment: [lejac, game, сlaws, home],
+	    area: 2.56,
+	    equipment: ["Лежак", "Игровой комплекс", "Когтеточка", "Домик"],
 	    price: 500
 	  },
 	  {
 	  	photo: "images/dest/catalog-img-6.jpg",
 	    name: "Супер-Люкс",
 	    size: '180x160x180',
-	    area: 2,88,
-	    equipment: [lejac, game, сlaws, home],
+	    area: 2.88,
+	    equipment: ["Лежак", "Игровой комплекс", "Когтеточка", "Домик"],
 	    price: 600
 	  },
 	];
 
-	// function age(birthYear) {
-	//   let calculatedAge = new Date().getFullYear() - birthYear;
-	//   if (calculatedAge == 1) {
-	//     return "1 year old";
-	//   } else if (calculatedAge == 0) {
-	//     return "Baby";
-	//   } else {
-	//     return `${calculatedAge} years old`;
-	//   }
-	// }
+	
 
-	// function foods(foods) {
-	//   return `
-	// <h4>Favorite Foods</h4>
-	// <ul class="foods-list">
-	// ${foods.map(food => `<li>${food}</li>`).join("")}
-	// </ul>
-	// `;
-	// }
+	function size(sizes) {
+	  return `<li><p>Размеры (ШxГxВ) - <span>${sizes}</span>см</p></li>`;
+	}
+	
+	function equipments(equipments) {
+		return `${equipments.map(equipment => {
+			if(equipment == 'Лежак'){
+		  		return `<img src="images/dest/equipment2.svg" alt="Лежак">`;
+		  	} else if(equipment == 'Игровой комплекс'){
+		  		return `<img src="images/dest/equipment3.svg" alt="Игровой комплекс">`
+		  	} else if(equipment == 'Когтеточка'){
+		  		return `<img src="images/dest/equipment4.svg" alt="Когтеточка">`
+		  	} else if(equipment == 'Домик'){
+		  		return `<img src="images/dest/equipment5.svg" alt="Домик">`
+		    }
+		}).join(" ")}`;
 
-	// function petTemplate(pet) {
-	//   return `
-	//     <div class="animal">
-	//     <img class="pet-photo" src="${pet.photo}">
-	//     <h2 class="pet-name">${pet.name} <span class="species">(${pet.species})</span></h2>
-	//     <p><strong>Age:</strong> ${age(pet.birthYear)}</p>
-	//     ${pet.favFoods ? foods(pet.favFoods) : ""}
-	//     </div>
-	//   `;
-	// }
+		
+			
+	}
 
-	// // document.getElementById("app").innerHTML = `
-	// //   <h1 class="app-title">Pets (${petsData.length} results)</h1>
-	// //   ${petsData.map(petTemplate).join("")}
-	// //   <p class="footer">These ${petsData.length} pets were added recently. Check back soon for updates.</p>
-	// // `;
+	function productTemplate(product) {
+	  return `
+
+	  	<div class="item">
+						<div class="product">
+							<div class="product-thumbnail">
+								<img src="${product.photo}" alt="Товар">
+							</div>
+							<div class="product-info">
+								<h3>${product.name}</h3>
+								<ul>
+									${product.size ? size(product.size) : ""}
+									<li><p>Площадь - <span class="area">${product.area}</span> м2</p></li>
+
+									<li><p>Оснащение номера <span>${product.equipment ? equipments(product.equipment) : '<img src="images/dest/equipment1.svg" alt="Пустой номер">'}</span></p></li>
+
+									<li><p>Цена за сутки <span class="price">${product.price}</span>₽</p></li>
+								</ul>
+								<a href="#" class="btn">Забронировать <i><img src="images/dest/lapa.svg" alt="Лапа кота"></i></a>
+							</div>
+						</div>
+					</div>
+	  `;
+	}
+
+	d.getElementById("resault").innerHTML = `${productData.map(productTemplate).join("")}`;
+
+
+
+
+	// филтр товаров
+
+
+	function getCheckedArea() {
+	  var checkboxes = document.getElementsByClassName('list-area-checkbox');
+	  var checkboxesChecked = []; // можно в массиве их хранить, если нужно использовать 
+	  for (var i = 0; i < checkboxes.length; i++) {
+	     if (checkboxes[i].checked) {
+	        checkboxesChecked.push(Number(checkboxes[i].name)); // положим в массив выбранный
+	        
+	     }
+	  }
+	  return checkboxesChecked; // для использования в нужном месте
+	}
+
+	function getCheckedEquipment() {
+	  var checkboxes = document.getElementsByClassName('list-equipment-checkbox');
+	  var checkboxesChecked = []; // можно в массиве их хранить, если нужно использовать 
+	  for (var i = 0; i < checkboxes.length; i++) {
+	     if (checkboxes[i].checked) {
+	        checkboxesChecked.push(checkboxes[i].name); // положим в массив выбранный
+	        
+	     }
+	  }
+	  return checkboxesChecked; // для использования в нужном месте
+	}
+
+	
+
+	var form = d.querySelector('.filter form');
+	form.onsubmit = function(e){
+		e.preventDefault();
+		var priceFrom = form.querySelector('#gap-from').value,
+			priceTo = form.querySelector('#gap-to').value,
+			listArea = getCheckedArea(),
+			listEquipment = getCheckedEquipment();
+
+			console.log(listArea)
+			console.log(listEquipment)
+
+
+			filterProductData = productData.slice();
+			//console.log(filterProductData)
+			for(var i = 0; i < filterProductData.length; i++) {
+
+				// Сверяем цену
+				if((filterProductData[i].price < priceFrom) || (filterProductData[i].price > priceTo) ){
+					console.log('Товар: '+i + ' Не подходит и был грохнут');
+					 delete filterProductData[i];
+				}
+
+				if (filterProductData[i] && listArea){
+					console.log("Товар выжыл")
+
+					for(var b = 0; b < listArea.length; b++){
+						if(Number(filterProductData[i].area) == Number(listArea[b])){
+							console.log('Товар: '+i + ' Подходит и он остается');
+							break;
+						}
+						if(b == listArea.length-1){
+							console.log('Товар: '+i + ' Не подходит и был грохнут | Залажал на площади');
+							delete filterProductData[i];
+						}
+					}
+				} else {
+					console.log("Товар" +i+ "мертв")
+
+				}
+
+				if (filterProductData[i] && listEquipment){
+					console.log("Товар выжыл")
+
+					for(var b = 0; b < listEquipment.length; b++){
+						console.log(filterProductData[i])
+						if((filterProductData[i].equipment) && (filterProductData[i].equipment.includes(listEquipment[b]))){
+							console.log('Товар: '+i + ' Подходит и он остается');
+							
+						}else{
+							console.log('Товар: '+i + ' Не подходит и был грохнут | Залажал на оснащении');
+							delete filterProductData[i];
+							break;	
+						}
+					}
+				} else {
+					console.log("Товар" +i+ "мертв")
+				}
+
+
+				// Сверяем площадь
+				
+
+				// Сверяем оснащение
+				// for(var b = 0; b < listArea.length; b++){
+				// 	if(Number(filterProductData[i].area) == Number(listArea[b])){
+				// 		console.log('Товар: '+i + ' Подходит и он остается');
+				// 		break;
+				// 	}
+				// 	if(b == listArea.length-1){
+				// 		console.log('Товар: '+i + ' Не подходит и был грохнут');
+				// 		delete filterProductData[i];
+				// 	}
+				// }
+
+				
+				
+		 	}
+
+		 	console.log(filterProductData)
+		 	d.getElementById("resault").innerHTML = `${filterProductData.map(productTemplate).join("")}`;
+		 	return ;
+		 	
+	}
 
 });
